c00f4e4b1e856c7a608253a3dd7d9f7e71850ef0
aagraston <aagraston@gmail.com>
2022-01-29 13:09:24 -0800

    manually reverted folders and files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import dash from 'lodash';
-import './index.css';
-import profile from './profile_pic.png';
 
 function component() {
   const element = document.createElement('div');
 
   //lodash manipulation
-  element.innerHTML = dash.join(['Hi', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const profilePic = new Image();
-  profilePic.src = profile;
-
-  element.appendChild(profilePic);
+  element.innerHTML = dash.join(['Roboto', 'webpack'], ' ');
   
   return element;
 }
