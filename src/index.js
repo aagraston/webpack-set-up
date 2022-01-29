82b8790f694e096e67bbc7653fd7a1a07f4352bc
aagraston <aagraston@gmail.com>
2022-01-29 13:46:16 -0800

    managing output with webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import dash from 'lodash';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   //lodash manipulation
-  element.innerHTML = dash.join(['Roboto', 'webpack'], ' ');
+  element.innerHTML = dash.join(['Hello', 'webpack'], ' ');
+
+btn.innerHTML = 'Click me and check the console!';
+btn.onclick = printMe;
+
+element.appendChild(btn);
   
   return element;
 }
